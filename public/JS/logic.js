e365451c12c11334249770318c1f9f8bd2927825
SamVerdiere <Samverdiere@gmail.com>
2021-12-22 03:14:21 +0100

    important functionalities

--- public/JS/logic.js
+++ public/JS/logic.js
@@ -3,6 +3,7 @@ const bod       = document.querySelector('body');
 const pagetitle = document.querySelector('.pageTitle');
 const pseudo    = document.querySelector('.nickname');
 const hautPage  = document.querySelector('.top');
+const subtitleTop  = document.querySelector('.subtitle');
 //table part
 const tableaux      = document.querySelector('.tables');
 const titreContact  = document.querySelector('.contactTitle');
@@ -19,20 +20,17 @@ const registerpart=document.querySelector('.partRegister');
 loginId.style.display = 'block';
 loginMdp.style.display= 'block';
 //btn create inside tables
-const createCtc = document.createElement('button');
-const createCtr = document.createElement('button');
-createCtc.innerHTML = 'Create Contact';
-createCtr.innerHTML = 'Create Contract';
-createCtc.className = 'createContact';
-createCtr.className = 'createContract';
+const createCtc = document.createElement('createContacte');
+const createCtr = document.querySelector('createContracte');
 var contactTable = document.getElementById('contacto');
 var contractTable = document.getElementById('contracta');
 
 /* Login part */
 //intial page setting
-pseudo.style.display = 'none';
 btnoff.style.display = 'none';
 tableaux.style.display = 'none';
+var inputlog  = loginId.value; 
+var inputpswd = loginMdp.value;
 
 //on click on "login" button :
 formlogin.addEventListener('submit', (e) => { 
@@ -44,18 +42,14 @@ formlogin.addEventListener('submit', (e) => {
     if(loginMdp.value === '') {
         alert('Please fill Id & Password fields.');
     } else if (loginId.value !== '' && loginMdp.value !== '') {
-    //if not, display elements And...
-    registerpart.style.display = 'none';
-    btnlog.style.display = 'none';
-    loginId.style.display = 'none';
-    loginMdp.style.display = 'none';
-    btnoff.style.display = 'block';
-    pseudo.style.display = 'inline';
-    pseudo.append(formcancel);
-    formcancel.appendChild(btnoff);
-    tableaux.style.display = 'block';
-    //allow connection And...
-    }
+    //if inputs are ok, allow connection and display elements...
+        let date = new Date(Date.now()+ 60000);
+        date = date.toUTCString();
+        displayElementsOnConfirm();
+        sessionStorage.setItem(loginId.value, loginMdp.value);
+        document.cookie = 'login='+loginId.value+';expires='+date+';secure';
+        document.cookie = 'password='+loginMdp.value+';expires='+date+';secure';
+    } 
     // $.ajax({
     //     url: 'path/to/server-side/script.php', /*url*/
     //     data: '', /* post data e.g name=christian&hobbie=loving */
@@ -67,44 +61,68 @@ formlogin.addEventListener('submit', (e) => {
     //     }
     // });
     //show Tables
-
 })
 
 //on click on log out button, get back to initial page:
 formcancel.addEventListener('cancel', (e) => {
     e.preventDefault();
+    document.cookie = 'login=; expires=; secure';
+    document.cookie = 'password=; expires=; secure';
+
+    sessionStorage.clear();
+    hideElementsOnCancel();
+})
+
+//helpers
+
+// function loginRequest() {
+//     let data = new FormData();
+//     data.append('name', loginId.value);
+//     data.append('pasword', loginMdp.value);
+        
+//     fetch('url', {
+//     method: 'POST',
+//     body: data
+//     })
+//     .then(data => data.text())
+//     .then((users) => findUser(users))
+// }
+
+
+
+function displayElementsOnConfirm() {
+    registerpart.style.display = 'none';
+    btnlog.style.display = 'none';
+    loginId.style.display = 'none';
+    loginMdp.style.display = 'none';
+    btnoff.style.display = 'block';
+    formcancel.appendChild(btnoff);
+    tableaux.style.display = 'block';
+    subtitleTop.innerHTML = 'Welcome to your personal interface,' +loginId.value+'. You are now logged in.'
+}
+
+function hideElementsOnCancel() {
     btnlog.style.display = 'inline';
     loginId.style.display = 'block';
     loginMdp.style.display = 'block';
     btnoff.style.display = 'none';
     pseudo.style.display = 'none';
-})
-
-//hide nickname before connection
-//connect on clic 
-// btnlog.click(function() {
-//     //send form with username+password to login.salesforce.com
-//     btnlog.css('display', 'none');
-//     sectionlogin.css('display', 'block');
-//     //if login success
-//     hautPage.append(sectionlogin);
-//     sectionlogin.append(loginform);
-//     pseudo.css('display', 'inline');
-// })
-
-// btnoff.click(function() {
-//     sectionlogin.css('display', 'none');
-//     pseudo.css('display', 'none');
-//     btnlog.css('display', 'inline');
-//     //send query to disconnect from Salesforce
-// })
+}
 
 /* Table part */
-titreContact.appendChild(createCtc);
-titreContract.appendChild(createCtr);
 
+/* CONTACTS */
 //get data; here in an object for example
-createCtc.addEventListener('click', function() { //get the list of contacts from DATABASe = same logic
+const inputContact = document.querySelector('.containsInput');
+const btnCreateCtc = document.querySelector('.createContacte');
+const btnSaveCtc = document.querySelector('.SaveContacte');
+inputContact.style.display = 'none';
+
+btnCreateCtc.addEventListener('click', function() { 
+inputContact.style.display = 'block';
+})
+
+btnSaveCtc.addEventListener('click', function() { //get the list of contacts from DATABASe = same logic
 var contactes = [
     {Name:'Testing 2', Email:'Mail Test2', Phone:'222-555-6'},
     {Name:'Testing 3', Email:'Mail Test3', Phone:'222-555-7'},
@@ -120,29 +138,42 @@ function createRow(contactes) {
     cell2.innerHTML = contactes.Email;
     var cell3 = row.insertCell(2);
     cell3.innerHTML = contactes.Phone;
+    inputContact.style.display = 'none';
    // contactTable.appendchild("<tr><td>"+contactes.Name+"</td><td>"+contactes.Email+"</td><td>"+contactes.Phone+"</td></tr>");
 }
 })
 
-createCtr.addEventListener('click', function () {
-var contracts = [
-    {Account:'GenePoint', Status:'Draft', StartDate:'16/12/2021', ContractTerm:'6'},
-    {Account:'Edge Communications', Status:'Activated', StartDate:'10/12/2021', ContractTerm:'12'},
-    {Account:'Dickenson plc', Status:'Draft', StartDate:'21/1/2022', ContractTerm:'10'},
-    {Account:'Pyramid Construction inc.', Status:'Activated', StartDate:'18/12/2021', ContractTerm:'9'}
-];
-contracts.forEach(createRows);
-function createRows(contracts) {
-    var row = contractTable.insertRow(-1);
-    var cell = row.insertCell(0);
-    cell.innerHTML = contracts.Account;
-    var cell2 = row.insertCell(1);
-    cell2.innerHTML = contracts.Status;
-    var cell3 = row.insertCell(2);
-    cell3.innerHTML = contracts.StartDate;
-    var cell4 = row.insertCell(3);
-    cell4.innerHTML = contracts.ContractTerm;
-}
+/* CONTRACTS */
+const inputContract = document.querySelector('.containsInpute');
+const btnCreateCtr = document.querySelector('.createContracte');
+const btnSaveCtr = document.querySelector('.SaveContracte');
+inputContract.style.display = 'none';
+
+btnCreateCtr.addEventListener('click', function () {
+inputContract.style.display = 'block';
+})
+
+btnSaveCtr.addEventListener('click', function() {
+    var contracts = [
+        {Account:'GenePoint', Status:'Draft', StartDate:'16/12/2021', ContractTerm:'6'},
+        {Account:'Edge Communications', Status:'Activated', StartDate:'10/12/2021', ContractTerm:'12'},
+        {Account:'Dickenson plc', Status:'Draft', StartDate:'21/1/2022', ContractTerm:'10'},
+        {Account:'Pyramid Construction inc.', Status:'Activated', StartDate:'18/12/2021', ContractTerm:'9'}
+    ];
+
+    contracts.forEach(createRows);
+    function createRows(contracts) {
+        var row = contractTable.insertRow(-1);
+        var cell = row.insertCell(0);
+        cell.innerHTML = contracts.Account;
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = contracts.Status;
+        var cell3 = row.insertCell(2);
+        cell3.innerHTML = contracts.StartDate;
+        var cell4 = row.insertCell(3);
+        cell4.innerHTML = contracts.ContractTerm;
+    }
+    inputContract.style.display = 'none';
 })
 
 //faire les requêtes AJAX:
